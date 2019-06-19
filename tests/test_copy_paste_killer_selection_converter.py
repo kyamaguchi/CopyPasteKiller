181685256557c2aac6bbb2133a2956a59db3f7fc
kyamaguchi <kzh.yap@gmail.com>
2019-06-20 01:35:15 +0900

    Refactor path of data files

--- tests/test_copy_paste_killer_selection_converter.py
+++ tests/test_copy_paste_killer_selection_converter.py
@@ -23,13 +23,16 @@ class TestCopyPasteKillerSelectionConverter(TestCase):
         self._run_test_with_scenario('tab_indent')
 
     def _run_test_with_scenario(self, scenario):
-        base_path = os.path.dirname(os.path.realpath(__file__))
-        with open(os.path.join(base_path, 'data', "{}.txt".format(scenario)), "r") as file:
+        with open(os.path.join(self._data_dir(), "{}.txt".format(scenario)), "r") as file:
             selection = file.read()
-        with open(os.path.join(base_path, 'data', "{}_find.txt".format(scenario)), "r") as file:
+        with open(os.path.join(self._data_dir(), "{}_find.txt".format(scenario)), "r") as file:
             expected_find_string = file.read()
-        with open(os.path.join(base_path, 'data', "{}_replace.txt".format(scenario)), "r") as file:
+        with open(os.path.join(self._data_dir(), "{}_replace.txt".format(scenario)), "r") as file:
             expected_replace_string = file.read()
         find_string, replace_string = CopyPasteKillerSelectionConverter.convert_for_find_and_replace(selection)
         self.assertEqual(find_string, expected_find_string)
         self.assertEqual(replace_string, expected_replace_string)
+
+    def _data_dir(self):
+        base_path = os.path.dirname(os.path.realpath(__file__))
+        return os.path.join(base_path, 'data')
