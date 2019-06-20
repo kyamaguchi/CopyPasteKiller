4393c8e03b3e79c6dc160379af2580a6ce4df109
kyamaguchi <kzh.yap@gmail.com>
2019-06-20 19:49:10 +0900

    Escape/unescape some special characters to make find & replace work

--- copy_paste_killer_selection_converter.py
+++ copy_paste_killer_selection_converter.py
@@ -8,9 +8,12 @@ class CopyPasteKillerSelectionConverter:
         lines = []
         for line in string.split("\n"):
             result = re.escape(re.sub(r'^\s+', "", line))
-            # Revert quotes to fix "Find"
+            # Revert quotes and some special characters to fix "Find"
             result = re.sub(r"\\'", "'", result)
             result = re.sub(r'\\"', '"', result)
+            result = re.sub(r'\\<', '<', result)
+            result = re.sub(r'\\>', '>', result)
+            result = re.sub(r'\\`', '`', result)
             lines.append(result)
         return "\n".join(["^([ \\t]*)" + l for l in lines])
 
@@ -19,5 +22,6 @@ class CopyPasteKillerSelectionConverter:
         lines = []
         for line in string.split("\n"):
             result = re.sub(r'^\s+', "", line)
+            result = result.replace('\\', '\\\\')
             lines.append(result)
         return "\n".join(["${}".format(i+1) + l for i, l in enumerate(lines)])
