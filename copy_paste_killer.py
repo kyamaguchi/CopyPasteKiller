e039d4ac09d11f2c87aa0a527ddd2b167cbe9aaf
kyamaguchi <kzh.yap@gmail.com>
2019-06-20 19:49:26 +0900

    Change the tile of tabs for buffer

--- copy_paste_killer.py
+++ copy_paste_killer.py
@@ -15,13 +15,13 @@ class CopyPasteKillerCommand(sublime_plugin.WindowCommand):
             selection = view.substr(view.line(view.sel()[0]))
 
         find_string = CopyPasteKillerSelectionConverter.convert_for_find(selection)
-        self._open_tab_with_find_string(find_string)
+        self._open_tab_with_find_string(find_string, "CopyPasteKiller (Find)")
 
         self.window.run_command('select_all')
         self.window.run_command('slurp_find_string')
 
         replace_string = CopyPasteKillerSelectionConverter.convert_for_replace(selection)
-        self._open_tab_with_find_string(replace_string)
+        self._open_tab_with_find_string(replace_string, "CopyPasteKiller (Replace)")
 
         self.window.run_command('select_all')
         self.window.run_command('slurp_replace_string')
@@ -32,9 +32,9 @@ class CopyPasteKillerCommand(sublime_plugin.WindowCommand):
         }
         self.window.run_command("show_panel", panel_args)
 
-    def _open_tab_with_find_string(self, selection):
+    def _open_tab_with_find_string(self, selection, name):
         v = self.window.new_file()
-        v.set_name("Find without indent")
+        v.set_name(name)
         v.set_scratch(True)
         v.assign_syntax('Packages/Regular Expressions/RegExp.sublime-syntax')
         v.run_command('append', {'characters': selection})
